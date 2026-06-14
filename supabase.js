a7619f7b7bbe06107f1389985af67e59a5bbc865
hous8281xfdv <eplaksin38@gmail.com>
2026-06-14 13:31:02 +0300

    Update supabase.js

--- supabase.js
+++ supabase.js
@@ -1,7 +1,6 @@
-{
-  "name": "novostnik",
-  "version": "1.0.0",
-  "dependencies": {
-    "@supabase/supabase-js": "^2.39.0"
-  }
-}
+import { createClient } from '@supabase/supabase-js';
+
+const supabaseUrl = 'https://iafkyliwyqaadwavaalu.supabase.co';
+const supabaseAnonKey = 'sb_publishable_FV7T0KpZp7bORF2Vd1r8_Q_L6SZn1f6';
+
+export const supabase = createClient(supabaseUrl, supabaseAnonKey);
