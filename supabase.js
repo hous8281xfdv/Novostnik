6d0c2402dfa6b820be786a66d0e2f56f61a57066
hous8281xfdv <eplaksin38@gmail.com>
2026-06-14 13:57:30 +0300

    Delete supabase.js

--- supabase.js
+++
@@ -1,6 +0,0 @@
-import { createClient } from 'https://cdn.skypack.dev/@supabase/supabase-js@2';
-
-const supabaseUrl = 'https://iafkyliwyqaadwavaalu.supabase.co';
-const supabaseAnonKey = 'sb_publishable_FV7T0KpZp7bORF2Vd1r8_Q_L6SZn1f6';
-
-export const supabase = createClient(supabaseUrl, supabaseAnonKey);
